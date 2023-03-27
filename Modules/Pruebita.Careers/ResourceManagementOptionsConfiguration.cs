ec63d645f9b1c4c48b40299373ff4509b9403799
Paola Vilaseca <paola.vilaseca.tx@gmail.com>
2023-03-27 10:23:50 -0400

    added style css file and lorem ipsum

---
+++ Modules/Pruebita.Careers/ResourceManagementOptionsConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Options;
+using OrchardCore.ResourceManagement;
+ 
+namespace Truextend.Careers;
+
+public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
+{
+    private static readonly ResourceManifest _manifest = new();
+    static ResourceManagementOptionsConfiguration() {
+        _manifest
+        .DefineStyle("Styles")
+        .SetUrl("..\\Pruebita.Web\\Css\\StyleSheet.css");
+    }
+    public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
+}
